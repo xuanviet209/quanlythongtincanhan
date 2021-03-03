a0586ee7c58a84ca9b8dbd58cb94dff5d24712f1
xuanviet209 <vietd8k11.gmail>
2021-03-03 09:31:08 +0700

    first commit

---
+++ themsuaxoa.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 03, 2021 lúc 03:25 AM
+-- Phiên bản máy phục vụ: 10.4.14-MariaDB
+-- Phiên bản PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `themsuaxoa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hocsinh`
+--
+
+CREATE TABLE `hocsinh` (
+  `id` int(11) NOT NULL,
+  `Username` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Email` varchar(120) COLLATE utf8_unicode_ci NOT NULL,
+  `Address` varchar(150) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `hocsinh`
+--
+
+INSERT INTO `hocsinh` (`id`, `Username`, `Email`, `Address`) VALUES
+(2, 'Nguyễn Xuân Kiên', 'kien204@gmail.com', 'Hà Nội'),
+(10, 'Nguyễn Hồng Hà', 'hongha1964@gmail.com', 'Xuân Đỉnh'),
+(11, 'Nguyễn Hồng Nhung', 'nhung1308@gmail.com', 'Xuân Đỉnh'),
+(13, 'Nguyễn Xuân Việt', 'vietd8k11@gmail.com', 'Hà Nội');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `hocsinh`
+--
+ALTER TABLE `hocsinh`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `hocsinh`
+--
+ALTER TABLE `hocsinh`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
